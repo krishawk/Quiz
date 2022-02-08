fa38e0172fb4070befeb07a56805269637f8ce3e
Kristie Hawkins <74780639+krishawk@users.noreply.github.com>
2022-02-08 11:05:47 -0800

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,11 +1,19 @@
 # We have to first think of questions to ask the user
-# we ask the user to select YES or No
 
+# Ask the user their name first
+name = input("Enter your name: ")
+
+# Then we set rules for this quiz. Only capital letters. NO LOWER CASE
+att_user = input("MAKE SURE YOUR ANSWERS ARE CAPITALIZED!!!" + " " "Press enter when ready to begin")
+
+
+# we ask the user to select YES or No
 question_1 = input("Does a dog bark?" + " " "Select YES or NO: ")
 
 question_2 = input("Does a chicken lay eggs?" + " " "Select YES or NO: ")
 
 question_3 = input("Can all birds fly?" + " " "Select YES or NO: ")
+
 
 # Now the computer lets you know how you did
 if question_1 == 'NO':
@@ -15,5 +23,5 @@ if question_2 == 'NO':
 if question_3 == 'YES':
     print("You got question 3 WRONG!")
 else:
-    print("YOU GOT THEM ALL RIGHT!! CONGRATS!!!!")
+    print("YOU GOT THEM ALL RIGHT!! CONGRATS!!!!" " "+name) # this way the computer prints the users name, so it feels more personal for the user
 
